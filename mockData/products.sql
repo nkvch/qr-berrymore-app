394914970af6d4a63c82f99d72f58ee2e7c814b4
Stanislau Stankevich <nkvch.st@gmail.com>
2022-08-10 13:48:54 +0200

    update sql mocks

--- mockData/products.sql
+++ mockData/products.sql
@@ -1,1 +1,1 @@
-insert into products (id, productName, productPrice) values (2, 'Blueberry', 10.5);
+INSERT INTO products (id, productName, productPrice, photoPath) VALUES (2, 'Blueberry', 10.5, '/savedFiles/productsPhotos/bfcd4c0d-7b4f-4285-b26a-734abefdaba1/blueberry.jpg');
--- mockData/users.sql
+++ mockData/users.sql
@@ -1,1 +1,4 @@
-insert into users (username, password, firstName, lastName, roleId) values ('demo', 'demo', 'Demo', 'Demo', 1);
+INSERT INTO users (id, username, password, email, firstName, lastName, roleId) VALUES (2, 'vova', '$2a$10$XKRCngdYYljXPpRn//wer.Oh17FOQr3/wEdn5Sv09MWEks/acr.ri', null, 'Joe', 'Biden', 2);
+INSERT INTO users (id, username, password, email, firstName, lastName, roleId) VALUES (3, 'dtrump', '$2a$10$9DuX7AK72p9uNHRjw41/peXBqrwr/fBoH74AgEJVahrM.UMtptrzm', null, 'Donald', 'Trump', 2);
+INSERT INTO users (id, username, password, email, firstName, lastName, roleId) VALUES (4, 'bobama', '$2a$10$YV699g3yuqGHMG6qMzaOFuBdglk3.jHGI8SoyMkgSqDVBFVVTPmYu', null, 'Barack', 'Obama', 2);
+INSERT INTO users (id, username, password, email, firstName, lastName, roleId) VALUES (5, 'demo', 'demo', null, 'Demo', 'Demo', 1);
